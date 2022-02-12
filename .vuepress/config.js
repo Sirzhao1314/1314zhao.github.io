cc75870c2aa61b48099648020c5acb9157992bd6
zhaoxing <vokeers@aliyun.com>
2022-02-12 13:35:02 +0800

    update blog

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -78,22 +78,16 @@ module.exports = {
       },
       "tag": {
         "location": 3,
-        "text": "便签"
+        "text": "标签"
       }
     },
     "friendLink": [
       {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
+        "title": "VOKE的酒馆",
+        "desc": "愿你永远少年，愿少年永远不缺乏勇气",
+        "email": "2558691827@qq.com",
+        "link": "https://blog.voke.work"
       },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
     ],
     "logo": "/logo.png",
     "search": true,
